e548be26e21b24caa5e28a7a0226f964535c0d7c
Shao <alex.shao.kahn@gmail.com>
2020-08-21 11:55:58 +0300

    update

--- src/com/shao/progkievua/homework2/lecture02/Board.java
+++ src/com/shao/progkievua/homework2/lecture02/Board.java
@@ -1,9 +1,14 @@
 package com.shao.progkievua.homework2.lecture02;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Board implements Serializable {
-    private Shape[] boardParts = new Shape[4];
+    private Shape[] boardParts;
+
+    public Board() {
+        boardParts = new Shape[4];
+    }
 
     public void addShape(Shape shape, int boardPart) {
         boardParts[boardPart] = shape;
@@ -11,6 +16,27 @@ public class Board implements Serializable {
 
     public void removeShape(int boardPart) {
         boardParts[boardPart] = null;
+    }
+
+    public Shape[] getBoardParts() {
+        return boardParts;
+    }
+
+    public void setBoardParts(Shape[] boardParts) {
+        this.boardParts = boardParts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return Arrays.equals(boardParts, board.boardParts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(boardParts);
     }
 
     @Override
