3f2e4d100d49dfa3e983c00b397a85673220d849
Shao <alex.shao.kahn@gmail.com>
2020-08-23 13:14:52 +0300

    FunctionalInterface

--- src/com/shao/progkievua/homework2/lecture04/MilitaryCommissar.java
+++ src/com/shao/progkievua/homework2/lecture04/MilitaryCommissar.java
@@ -2,6 +2,7 @@ package com.shao.progkievua.homework2.lecture04;
 
 import com.shao.progkievua.homework2.lecture03.Student;
 
+@FunctionalInterface
 public interface MilitaryCommissar {
     Student[] getRecruitsList();
 }
